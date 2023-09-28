bb643bb1cbb2e22283edec66185e112705be4e4c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-28 20:12:00 +0100

    bug fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
