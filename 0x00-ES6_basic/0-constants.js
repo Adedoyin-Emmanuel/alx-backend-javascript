bfa671eb9beb282878fbd29273775a2dcf2c7cc0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-28 18:30:34 +0100

    Finished first task

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+export function taskFirst() {
+  const task = "I prefer const when I can.";
+  return task;
+}
+
+
+export function taskNext() {
+  let combination = "But sometimes let";
+  combination += getLast();
+
+  return combination;
+}
