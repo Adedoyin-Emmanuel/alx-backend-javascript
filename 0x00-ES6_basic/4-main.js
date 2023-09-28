82caf04b37c5b1919b8e1bb9e3b1029bb9ed037c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-28 21:16:09 +0100

    Fixed eslint errors

--- 0x00-ES6_basic/4-main.js
+++ 0x00-ES6_basic/4-main.js
@@ -1,4 +1,4 @@
-import returnHowManyArguments from "./4-rest-parameter.js";
+import returnHowManyArguments from './4-rest-parameter';
 
-console.log(returnHowManyArguments("one"));
-console.log(returnHowManyArguments("one", "two", 3, "4th"));
+console.log(returnHowManyArguments('one'));
+console.log(returnHowManyArguments('one', 'two', 3, '4th'));
