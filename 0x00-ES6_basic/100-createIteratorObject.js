385268ef54a4be636ff9fff63be4d19f9e40e233
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-29 05:06:12 +0100

    Fixed eslint errors

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,32 +1,9 @@
 export default function createIteratorObject(report) {
-  const employees = report.allEmployees;
-  const department = Object.keys(employees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+  const array = [];
 
-  return {
-    next() {
-      if (
-        currentEmployeeIndex >=
-        employees[department[currentDepartmentIndex]].length
-      ) {
-        currentDepartmentIndex += 1;
-        currentEmployeeIndex = 0;
-      }
+  for (const item of Object.values(report.allEmployees)) {
+    array.push(item);
+  }
 
-      if (currentDepartmentIndex >= department.length) {
-        return { done: true };
-      }
-
-      const result = {
-        value:
-          employees[department[currentDepartmentIndex]][currentEmployeeIndex],
-        done: false,
-      };
-
-      currentEmployeeIndex += 1;
-
-      return result;
-    },
-  };
+  return array;
 }
