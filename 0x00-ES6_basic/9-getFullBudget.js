fc52c85f2766fc293abb4f2b27d72dcbe1a075b1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-29 04:04:45 +0100

    Fixed eslint errors

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,13 +1,13 @@
-import getBudgetObject from "./7-getBudgetObject.js";
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
