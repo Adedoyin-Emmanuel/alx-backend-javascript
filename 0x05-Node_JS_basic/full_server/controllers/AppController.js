dd47c172d6c64dcc343e39c97726d8de9bbd17a0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-18 05:42:25 +0100

    Updates

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * Contains route handlers.
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
