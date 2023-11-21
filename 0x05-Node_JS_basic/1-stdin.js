b0507e153cc76e15c0ae6dfcf1991efbf7e48031
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-21 03:51:44 +0100

    Fix: Fixed 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const name = process.stdin.read();
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
