b8ffd778976673d5f7f0d02defc0015c8db99009
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-21 04:02:10 +0100

    Feat: Added deps

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
