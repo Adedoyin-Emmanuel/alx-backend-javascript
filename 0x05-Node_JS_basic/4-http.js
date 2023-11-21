fd78fafaca8e701085d799b0e02bfd1197d03a52
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-21 03:56:08 +0100

    Fix: Fixed 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -14,7 +14,7 @@ app.on('request', (_, res) => {
 });
 
 app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+  process.stdout.write(`Server listening @ http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
